7e8c5a5a96f48407e6a53c6202c45e003f172012
JamesSand <shazz20@mails.tsinghua.edu.cn>
2022-12-05 20:36:19 +0800

    final 19M

--- thinpad_top.ip_user_files/ip/pll_example/pll_example_stub.v
+++ thinpad_top.ip_user_files/ip/pll_example/pll_example_stub.v
@@ -1,7 +1,7 @@
 // Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
 // --------------------------------------------------------------------------------
 // Tool Version: Vivado v.2019.2 (win64) Build 2708876 Wed Nov  6 21:40:23 MST 2019
-// Date        : Mon Dec  5 20:27:34 2022
+// Date        : Sun Dec  4 23:17:09 2022
 // Host        : LAPTOP-8A2NUVFV running 64-bit major release  (build 9200)
 // Command     : write_verilog -force -mode synth_stub
 //               D:/2022SecondHalf/2022Fall/ComputerOriganization/cod22-grp55/thinpad_top.srcs/sources_1/ip/pll_example/pll_example_stub.v
